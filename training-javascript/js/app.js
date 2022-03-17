70d13deb4381414037e67b78ee0ce3dfc88d3182
Diego <diego_tapia_millan@yahoo.es>
2022-03-17 20:43:56 +0100

    inicio js

---
+++ training-javascript/js/app.js
@@ -0,0 +1,8 @@
+esJueves = true;
+number2 = "Diego&Tapia";
+number3 = -10;
+number4 = 789.14;
+console.log(typeof esJueves);
+console.log(typeof number2);
+console.log(typeof number3);
+console.log(number4);
