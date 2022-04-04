11be74e9da2189ba28ca9f79888aaf264edcec94
Diego <diego_tapia_millan@yahoo.es>
2022-04-04 11:54:32 +0200

    some practice using code js for moving through the text

---
+++ ejercicios html-css fácil M2/proyecto login-reg/login.js
@@ -0,0 +1,12 @@
+// let box1 = document.body.querySelector(".box1");
+// console.log = (box1)
+// let allDivs = document.querySelectorAll(`div, form, button`);
+// console.log = (allDivs[1].firstElementChild.innerText);
+// document.body.children[0].children[0].children[0].innerText = "jamón"
+// document.body.firstElementChild.firstElementChild.nextElementSibling.previousElementSibling.parentNode.innerText = "jamon"
+// document.body.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.innerText = "jamón".
+let box2 = document.querySelector(".box2");
+console.log(box2)
+console.log(document.body.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild)
+console.log(document.body.children[0].children[4].firstElementChild.parentNode.parentNode.parentNode.nodeName)
+console.log(document.body.firstElementChild.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild.firstChild.nodeValue)
